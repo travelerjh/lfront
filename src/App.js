517a4a9444e66e260a7ef22a7b03aa130aa0f321
jaehee <cjhee0601@naver.com>
2023-12-29 15:42:35 +0900

    git 에올리기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { HomeLayout } from "./layout/HomeLayout";
+import { BoardWrite } from "./page/BoardWrite";
+import { BoardList } from "./page/BoardList";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />}>
+      <Route index element={<BoardList />} />
+      <Route path="write" element={<BoardWrite />} />
+    </Route>,
+  ),
+);
 
 function App(props) {
-    return (
-        <div></div>
-    );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
